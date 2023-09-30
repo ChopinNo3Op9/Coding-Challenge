0e502abbffd6c45ace6750b3418d65593a25b58a
ChopinNo3Op9 <67731233+ChopinNo3Op9@users.noreply.github.com>
2023-09-30 10:36:39 -0400

    new

--- rearranging words.py
+++ rearranging words.py
@@ -1,16 +1,15 @@
 from itertools import permutations
 
+def rearrange (word):
+    permuted_words = [''.join(p) for p in permutations(word)]
+    unique_permutations = sorted(set(permuted_words))
+    
+    if unique_permutations[-1] == word:
+        return("no answer")
+    else:
+        return(unique_permutations[unique_permutations.index(word) + 1])
+
+# test
 # word = "cbaa"
 word = "baca"
-
-permuted_words = [''.join(p) for p in permutations(word)]
-
-unique_permutations = sorted(set(permuted_words))
-
-print(unique_permutations)
-
-if unique_permutations[-1] == word:
-    print("no answer")
-else:
-    print(unique_permutations[unique_permutations.index(word) + 1])
-
+print(rearrange(word))
