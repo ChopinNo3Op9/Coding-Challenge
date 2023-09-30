2710d93c534ed7ca63a9c78300a4268c660e66e7
ChopinNo3Op9 <67731233+ChopinNo3Op9@users.noreply.github.com>
2023-09-30 10:51:15 -0400

    add

---
+++ maximum occurrence.py
@@ -0,0 +1,23 @@
+def max_occurrence_first(string):
+
+    letter_count = {}
+
+    for s in string:
+        if s in letter_count:
+            letter_count[s] += 1
+        else:
+            letter_count[s] = 1
+
+    max_letter = None
+    max_count = 0
+    for letter, count in letter_count.items():
+        if count > max_count:
+            max_letter = letter
+            max_count = count
+
+    return max_letter, max_count
+    
+# test
+input_string = "heeeelllloo"
+result_char, result_count = max_occurrence_first(input_string)
+print(result_char)
